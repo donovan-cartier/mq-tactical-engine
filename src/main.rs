f36a98f8a0ad281c5f5057e380e2e3368d697fd4
donovan-cartier <donovan.cartier0@gmail.com>
2024-07-01 19:53:17 +0200

    Added egui, sidepanel, topbar

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,72 @@
 mod scene;
 
+use egui::Align2;
 use macroquad::prelude::*;
 use scene::{Scene, Entity};
 
 #[macroquad::main("MQ TACTICAL ENGINE")]
 async fn main() {
+    // Persistent editor UI
+
+    let mut show_about_window = false;
+
+
+    // Temp scene saving
     let ett = Entity{name: String::from("Test Entity"), id: 1};
 
-    let scn = Scene {
+    Scene::load(String::from("Game.json"));
+
+    let mut scn = Scene {
         entities : vec!{ett},
-        name: String::from("Game")
+        name: String::from("Game"),
+        path: String::new()
     };
     
     scn.save();
 
+
+    // editor
+    
     loop {
         clear_background(BLACK);
-
+        
         draw_text("MQ TACTICAL ENGINE", 20.0, 20.0, 30.0, WHITE);
+        egui_macroquad::ui(|egui_ctx| {
+
+            
+            
+            egui::TopBottomPanel::top("action_bar").show(egui_ctx,|ui| {
+                ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| {
+                    ui.button("File");
+                    ui.button("Edit");
+                    
+                    
+                    let about = ui.button("About");
+                    
+                    if about.clicked(){
+                        show_about_window = true;
+                    }
+
+                    if show_about_window == true{
+                        egui::Window::new("About")
+                            .anchor(Align2::CENTER_CENTER, [0.0,0.0])
+                            .collapsible(false)
+                            .resizable(false)
+                            .open(&mut show_about_window)
+                            .show(egui_ctx, |ui|{
+                            ui.label("MQ Tactical Engine - indev");
+                        });
+                    }
+                })
+             });
+
+            egui::SidePanel::new(egui::panel::Side::Left, egui::Id::new("scene_tree")).show(egui_ctx, |ui|{
+                ui.label("Scene Tree");
+            });
+        });
+
+
+        egui_macroquad::draw();
 
         next_frame().await
     }
--- src/scene.rs
+++ src/scene.rs
@@ -10,12 +10,22 @@ pub struct Entity {
 pub struct Scene {
     pub entities: Vec<Entity>,
     pub name: String,
+    pub path: String,
 }
 
 
 impl Scene {
-    pub fn save(&self){
+    pub fn save(&mut self){
         let serialized_scene = serde_json::to_string(self).unwrap();
         std::fs::write(String::from(&self.name) + ".json", serialized_scene).unwrap();
+        self.path = String::from(&self.name);
+    }
+
+    pub fn load(path: String){
+        let scene = std::fs::read(path).unwrap();
+        // Implement file check here
+
+        println!("{}", String::from_utf8(scene).unwrap());
+        // println!("loading {scene_name} from {scene_path}", scene_name = self.name, scene_path = self.path);
     }
 }
